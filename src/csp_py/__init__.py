ad3d793eaf3536877fc4e5331ae0e5b7c12ae98e
Maciej Nowak <mnowak@kplabs.pl>
2026-06-02 14:40:40 +0200

    feat: CSP address model

--- src/csp_py/__init__.py
+++ src/csp_py/__init__.py
@@ -2,11 +2,13 @@ from .packet import CspId, CspPacket, CspPacketPriority, CspPacketFlags
 from .router import CspRouter
 from .node import CspNode
 from .packet_handler import IPacketHandler
+from .address import CspAddress
 
 from .services.ping_client import ping
 
 
 __all__ = [
+    'CspAddress',
     'CspId',
     'CspPacket',
     'CspPacketPriority',
